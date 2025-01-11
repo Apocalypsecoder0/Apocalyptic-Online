18f455914e8ef34226c73a40c2b8ca903c0c8e65
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-10 22:33:45 -0500

    Update Mercenaries.php

--- Unit-management/Mercenaries.php
+++ Unit-management/Mercenaries.php
@@ -1,1 +1,18 @@
+<?php
+// Mercenary Management
+class Mercenaries {
+    public function hireMercenaries($quantity) {
+        // Logic to hire mercenaries
+        return "$quantity mercenaries hired!";
+    }
 
+    public function deployMercenaries($target) {
+        // Logic to deploy mercenaries
+        return "Mercenaries deployed to $target!";
+    }
+}
+
+// Example Usage
+$mercenaries = new Mercenaries();
+echo $mercenaries->hireMercenaries(200);
+?>

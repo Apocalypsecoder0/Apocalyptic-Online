ec9f93affd06d242432e9a26d52841d755312f46
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-10 22:34:52 -0500

    Update Intelligene.php

--- Unit-management/Intelligene.php
+++ Unit-management/Intelligene.php
@@ -1,1 +1,13 @@
+<?php
+// Intelligence Management
+class Intelligence {
+    public function upgradeSpyLevel($level) {
+        // Logic to upgrade spy level
+        return "Spy level upgraded to $level!";
+    }
+}
 
+// Example Usage
+$intelligence = new Intelligence();
+echo $intelligence->upgradeSpyLevel(5);
+?>

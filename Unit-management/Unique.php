c11600614fe230d5f3f9722310c06adf30b7f477
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-10 22:34:18 -0500

    Update Unique.php

--- Unit-management/Unique.php
+++ Unit-management/Unique.php
@@ -1,1 +1,13 @@
+<?php
+// Unique Unit Management
+class UniqueUnits {
+    public function unlockUniqueUnit($unitName) {
+        // Logic to unlock a unique unit
+        return "Unique unit $unitName unlocked!";
+    }
+}
 
+// Example Usage
+$unique = new UniqueUnits();
+echo $unique->unlockUniqueUnit("Dragon Knight");
+?>

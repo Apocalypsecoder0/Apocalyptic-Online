df2d85b7ba62e42b9b10957140258138dcd4f6b1
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-10 23:12:47 -0500

    Update and rename Text to Db.php

---
+++ IncludeS/Db.php
@@ -0,0 +1,15 @@
+
+<?php
+// Database connection
+$host = 'localhost';
+$db_name = 'ogame';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
